fd471bc78d72320b5b23ca243ac32f99e8cf8d8e
[Vishwam Shah] <[shahvishwam7@gmail.com]>
2021-05-09 15:23:47 +0530

    Hero effect to image transition to details page

--- lib/shared/tripList.dart
+++ lib/shared/tripList.dart
@@ -47,9 +47,12 @@ class _TripListState extends State<TripList> {
       ),
       leading: ClipRRect(
         borderRadius: BorderRadius.circular(8.0),
-        child: Image.asset(
-          'images/${trip.img}',
-          height: 50.0,
+        child: Hero(
+          tag: 'location-tag-${trip.img}',
+          child: Image.asset(
+            'images/${trip.img}',
+            height: 50.0,
+          ),
         ),
       ),
       trailing: Text('\$${trip.price}'),
